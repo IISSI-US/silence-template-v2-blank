ca32fcf2758f472628662bc472ff354fdc0908ea
Agustin Borrego <borrego@us.es>
2022-03-30 11:58:12 +0200

    Updated template to use Bootstrap v5

--- web/js/utils/include.js
+++ web/js/utils/include.js
@@ -3,8 +3,8 @@
 function include(htmlFile, selector) {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", htmlFile, false); // Becomes synchronous
-    xhr.addEventListener("load", () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
+    xhr.addEventListener("load", function() {
+        if (xhr.status === 200) {
             document.querySelector(selector).innerHTML += xhr.responseText;
         } else {
             console.error(htmlFile + " does not exist");
